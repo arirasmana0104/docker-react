c85916262cf8ab423efdb69e60b8a6795f9ea5c1
arirasmana0104 <90232090+arirasmana0104@users.noreply.github.com>
2026-09-11 18:27:23 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I LOVE YOU ADEK ADIKAAA SAYANGGGGGG
+          I LOVE YOU ADIKAA
         </a>
       </header>
     </div>
